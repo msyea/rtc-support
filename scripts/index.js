1716434df4023a67677ce69bb411b19e9a2430eb
Simon Mayes <simon.mayes@bglgroup.co.uk>
2019-01-11 16:13:36 +0000

    Add icon shiz

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,52 @@
+const cursorPng = require("./icons").cursor;
+
 (async () => {
   const state = {
     initialised: false,
     activeDc: null
   };
-  const rtcConfig = {'iceServers': [{'url': "stun:stun.gmx.net"}]}
+  let cursor;
+  const setupPlugins = () => {
+    console.log('To send a message use "chat `hello`" ');
+    window.sendMessage = message => {
+      console.log(`[You] ${message}`)
+      state.activeDc.send(JSON.stringify({ message }));
+    };
+    window.chat = messageTpl => {
+      console.log(`[You] ${messageTpl[0]}`)
+      state.activeDc.send(JSON.stringify({ message: messageTpl[0] }));
+    };
+    cursor = Object.assign(document.createElement("img"), {
+      src: cursorPng
+    });
+    cursor.style.position = "absolute";
+    cursor.style.height = "25px";
+    cursor.style.width = "25px";
+    document.body.appendChild(cursor);
+    document.addEventListener("mousemove", evt => {
+      state.activeDc.send(
+        JSON.stringify({
+          type: "mousemove",
+          clientX: evt.clientX,
+          clientY: evt.clientY
+        })
+      );
+    });
+  };
+
+  const onMessage = evt => {
+    const payload = JSON.parse(evt.data);
+    if ("mousemove" === payload.type) {
+      cursor.style.left = `${payload.clientX}px`;
+      cursor.style.top = `${payload.clientY}px`;
+    } else if (payload.message) {
+      console.log(`[Partner] ${payload.message}`)
+    } else {
+      console.log(evt)
+    }
+  };
+
+  const rtcConfig = { iceServers: [{ url: "stun:stun.gmx.net" }] };
   const pc1 = new RTCPeerConnection(rtcConfig);
   let dc1;
   pc1.addEventListener("icecandidate", async evt => {
@@ -19,7 +62,6 @@
       await pc1.setRemoteDescription(answerDesc);
     }
   });
-  pc1.addEventListener("message", console.log);
   const pc2 = new RTCPeerConnection(rtcConfig);
   let dc2;
   pc2.addEventListener("icecandidate", evt => {
@@ -30,34 +72,37 @@
       );
     }
   });
-  pc2.addEventListener("message", console.log);
   pc2.addEventListener("datachannel", evt => {
     dc2 = evt.channel || evt;
     state.activeDc = dc2;
-    dc2.addEventListener("open", () => console.log("dc2 open"));
-    dc2.addEventListener("message", console.log);
-    window.sendMessage = message => {
-      dc2.send(JSON.stringify({ message }));
-    };
+    dc2.addEventListener("open", () => {
+      console.log("dc2 open");
+      setupPlugins();
+    });
+    dc2.addEventListener("message", onMessage);
   });
   const requestOrRespond = confirm(
-    ["Welcome to P2P Bookmarklet!",
-    "First of all you need to exchange tokens with your partner.",
-    "Do you want to create a new token?",
-    "Or click [Cancel] if someone sent you a token."].join("\n")
+    [
+      "Welcome to P2P Bookmarklet!",
+      "First of all you need to exchange tokens with your partner.",
+      "Do you want to create a new token?",
+      "Or click [Cancel] if someone sent you a token."
+    ].join("\n")
   );
   if (requestOrRespond) {
     dc1 = pc1.createDataChannel("test", { reliable: true });
     state.activeDc = dc1;
-    dc1.addEventListener("open", () => console.log("dc1 open"));
-    dc1.addEventListener("message", console.log);
-    window.sendMessage = message => {
-      dc1.send(JSON.stringify({ message }));
-    };
+    dc1.addEventListener("open", () => {
+      console.log("dc1 open");
+      setupPlugins();
+    });
+    dc1.addEventListener("message", onMessage);
     const offer = await pc1.createOffer();
     await pc1.setLocalDescription(offer);
   } else {
-    const offer = prompt("Please enter the token your partner shared with you and click [OK].");
+    const offer = prompt(
+      "Please enter the token your partner shared with you and click [OK]."
+    );
     var offerDesc = new RTCSessionDescription(JSON.parse(atob(offer)));
     await pc2.setRemoteDescription(offerDesc);
     const answerDesc = await pc2.createAnswer();
